0520c6466e29c9a0f5b1473bd32975f5df37c954
Khevynn Sá <115984821+Khevynn@users.noreply.github.com>
2023-11-20 19:13:57 +0000

    Pause menu
    - Pause menu Added (Needs art, but it is for 3rd delivery)

---
+++ Assets/Scripts/GUIController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUIController : MonoBehaviour
+{
+    [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject _pauseMenu;
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && _pauseMenu.activeSelf == false)
+        {
+            _pauseMenu.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && _pauseMenu.activeSelf == true)
+        {
+            _pauseMenu.SetActive(false);
+            Time.timeScale = 1.0f;
+        }
+    }
+
+    public void GoBackToMainMenu(string saveConfirm)
+    {
+        if(saveConfirm == "Yes")
+        {
+            SaveLoad.OverwriteSave();
+        }
+
+        FindAnyObjectByType<SceneLoader>().LoadScene("MainMenuScene");
+        Time.timeScale = 1.0f;
+    }
+
+    public void Continue()
+    {
+        Time.timeScale = 1.0f;
+    }
+
+    public void ResetPlayer()
+    {
+        StartCoroutine(_player.GetComponent<PlayerController>().Death());
+        Time.timeScale = 1.0f;
+    }
+}
